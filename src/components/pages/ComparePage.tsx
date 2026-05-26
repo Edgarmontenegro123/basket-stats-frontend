aea2bb855ad61e16b03051d77b204ef768ccae14
Edgar <edgarmontenegro321@gmail.com>
2026-05-26 14:25:43 -0300

    🧑‍💻 Add team selectors to compare page

--- src/components/pages/ComparePage.tsx
+++ src/components/pages/ComparePage.tsx
@@ -1,8 +1,36 @@
+import {useEffect, useState} from 'react'
 import PageHeader from '../common/PageHeader'
 import SectionCard from '../common/SectionCard'
+import type {Team} from '../types/team'
+import {getTeams} from '../services/api'
 import '../common/PageLayout.css'
 
 const ComparePage = () => {
+    const [teams, setTeams] = useState<Team[]>([])
+    const [teamAId, setTeamAId] = useState('')
+    const [teamBId, setTeamBId] = useState('')
+
+    useEffect(() => {
+        const fetchTeams = async () => {
+            try {
+                const data = await getTeams()
+                setTeams(data)
+
+                if (data.length > 0) {
+                    setTeamAId(data[0].id)
+                }
+
+                if (data.length > 1) {
+                    setTeamBId(data[1].id)
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        void fetchTeams()
+    }, []);
+
     return (
         <div>
             <PageHeader
@@ -11,7 +39,37 @@ const ComparePage = () => {
             />
 
             <SectionCard title='Comparison'>
-                <p>Select teams to compare their stats.</p>
+                <div className='form-row'>
+                    <div className='form-group'>
+                        <label>Team A</label>
+                        <select
+                            className='form-select'
+                            value={teamAId}
+                            onChange={(event) => setTeamAId(event.target.value)}
+                        >
+                            {teams.map((team) => (
+                                <option key={team.id} value={team.id}>
+                                    {team.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className='form-group'>
+                        <label>Team B</label>
+                        <select
+                            className='form-select'
+                            value={teamBId}
+                            onChange={(event) => setTeamBId(event.target.value)}
+                        >
+                            {teams.map((team) => (
+                                <option key={team.id} value={team.id}>
+                                    {team.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
             </SectionCard>
         </div>
     )
